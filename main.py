655afc8e0a2476e4a451a35933583425cdca61c6
Adrià Alfonso Medero <90824134+adrialfonso@users.noreply.github.com>
2024-01-24 17:36:28 +0100

    feature(): first enemy added (SquareEnemy)

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 import sys
+import random
+import time
 from character import Character
+from circle_enemy import CircleEnemy
 
 # Initialize Pygame
 pygame.init()
@@ -13,9 +16,14 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Agony Arcade")
 
 # Create a Character instance
-ball = Character(width // 2, height // 2, 5)
+character = Character(width // 2, height // 2, 5)
+
+# List to store enemies
+num_enemies = 7
+enemies = [CircleEnemy(random.randint(0, width), random.randint(0, height), 5) for _ in range(num_enemies)]
 
 # Main loop
+clock = pygame.time.Clock()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,14 +31,30 @@ while True:
             sys.exit()
 
     keys = pygame.key.get_pressed()
-    ball.move(keys)
+    character.move(keys)
 
-    # Draw on the screen
-    screen.fill((255, 255, 255))  # Fill the screen with white color
-    ball.draw(screen)  # Draw the character
+    # Move and check collisions for each enemy
+    for enemy in enemies:
+        enemy.move(character)
+
+    # Check collisions after all enemies have moved
+    collision = any(enemy.check_collision(character) for enemy in enemies)
+    if collision:
+        time.sleep(0.3)
+        sys.exit()
+    else:
+        screen.fill((255, 255, 255))  # Fill the screen with white color
+
+    # Draw character
+    character.draw(screen)
+
+    # Draw and update all enemies
+    for enemy in enemies:
+        enemy.draw(screen)
 
     # Update the display
     pygame.display.flip()
 
     # Control the update speed
-    pygame.time.Clock().tick(60)
+    clock.tick(60)
+
